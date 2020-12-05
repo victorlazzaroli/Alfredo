01295a0b30137045acb17b874ac648b48f37d4f5
Victor <the_wizard_ita@hotmail.it>
2020-12-05 14:58:11 +0100

    some components creations

--- assenzeManager/src/app/app.module.ts
+++ assenzeManager/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './core/authentication/authentication.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
